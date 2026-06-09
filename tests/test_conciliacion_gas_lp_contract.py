8e85b5c876de0640089a0515bff53342be687c3d
chemamaria74-lab <chemamaria74@gmail.com>
2026-06-09 16:44:08 -0600

    Add files via upload

--- tests/test_conciliacion_gas_lp_contract.py
+++ tests/test_conciliacion_gas_lp_contract.py
@@ -1255,6 +1255,9 @@ def test_carta_porte_xml_adds_seconds_to_browser_datetime_values():
     )
 
     assert 'TipoDeComprobante="T"' in xml
+    assert 'SubTotal="0" Total="0" Moneda="XXX"' in xml
+    assert '<cfdi:Concepto ClaveProdServ="78101800"' in xml
+    assert 'BienesTransp="15111510"' in xml
     assert 'Fecha="2026-06-09T14:32:00"' in xml
     assert 'PlacaVM="AC6116E"' in xml
     assert 'PlacaVM="AC-6116-E"' not in xml
