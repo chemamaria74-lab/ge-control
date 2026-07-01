b524217bd669ea98b4a37108cf8749100c3ff9b4
chemamaria74-lab <chemamaria74@gmail.com>
2026-07-01 11:22:57 -0600

    Add files via upload

--- static/js/gas_lp/conciliacion/00_state.js
+++ static/js/gas_lp/conciliacion/00_state.js
@@ -1,7 +1,7 @@
 const CONCILIACION_TOKEN_KEY='ge_gaslp_conciliacion_token';
 const token=localStorage.getItem(CONCILIACION_TOKEN_KEY)||'';
 const PROFILE_KEY='ge_gaslp_conciliacion_perfil_id';
-let FACTURAS=[], FACILITIES=[], PERFILES=[], COMPLEMENTOS=[], CLIENTES=[], SEL={}, COMP_CONFIRM_CTX=null, CRED_CLIENT_KEY='', PUB_CONFIRM_RESOLVER=null, PUB_FINAL_PAYLOAD=null, PUBLICO_COMPANY_PRICE=0;
+let FACTURAS=[], PPD_PENDIENTES=[], FACILITIES=[], PERFILES=[], COMPLEMENTOS=[], CLIENTES=[], SEL={}, COMP_CONFIRM_CTX=null, CRED_CLIENT_KEY='', PUB_CONFIRM_RESOLVER=null, PUB_FINAL_PAYLOAD=null, PUBLICO_COMPANY_PRICE=0;
 let CANCEL_CTX=null;
 let BANK_RECONCILIATION_CTX=null;
 let activePerfilId=Number(localStorage.getItem(PROFILE_KEY)||'0')||null;
